e2de4fd29a9d570195f8b2f0979f7afa64d4c1c8
Miguel Ceriani <miguel.ceriani@gmail.com>
2021-02-16 20:03:16 +0100

    Add translation of RDF JS terms to JSON-LD

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import { translate } from 'sparqlalgebrajs-input';
+import {translate} from 'sparqlalgebrajs-input';
+import {translateTerms} from './rdfJsToJsonLd';
 
 import * as jsonld from 'jsonld';
 
 export async function SPARQLtoJsonLd(sparqlString: String) {
     const algebra = translate(sparqlString);
-    return algebra;
+    const algebraWithJsonLdTerms = await translateTerms(algebra);
+    return algebraWithJsonLdTerms;
 }
 
 export function SPARQLtoRDF() {
--- src/rdfJsToJsonLd.ts
+++ src/rdfJsToJsonLd.ts
@@ -1,5 +1,25 @@
 import {Term} from "rdf-js";
+import {Util} from "jsonld-streaming-serializer";
+import {ContextParser} from "jsonld-context-parser";
+import { Algebra } from 'sparqlalgebrajs-input';
 
-export function translate(term: Term) {
-    
+const ctxtParser = new ContextParser();
+
+export async function translateTerms(operation: Algebra.Operation): Promise<Algebra.Operation> {
+    const ctxt = await ctxtParser.parse({});
+    function translateObj(obj: any): any {
+        if (typeof obj !== 'object' || obj === null) {
+            return obj;
+        }
+        if (Array.isArray(obj)) {
+            console.log('array');
+            return obj.map(i => translateObj(i));
+        }
+        if (obj.termType && obj.termType !== 'Quad' && obj.termType !== 'Variable' && obj.termType !== 'DefaultGraph') {
+            console.log('term');
+            return Util.termToValue(obj, ctxt);
+        } 
+        return Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, translateObj(v)]));
+    }
+    return translateObj(operation);
 }
